4cd419609fa5c295f2976f37d6c3faedba5247c2
Fredrik <fredrik.rundh@betssongroup.com>
2019-10-03 18:29:05 +0200

    simple poc

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-k3s-teamcity
+# TeamCity Agents in K3S
+PoC of TC agents running as Pods in K3S cluster with DooD support
+
+## TeamCity Agent Docker Image
+### Build Image
+```
+docker build -t frundh/tc-agent --build-arg TC_AGENT_ZIP=https://teamcity.frundh.local/update/buildAgent.zip .
+```
+Based on https://github.com/JetBrains/teamcity-docker-minimal-agent/blob/master/ubuntu/Dockerfile
+
+## K3S Cluster
+### Create
+```
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--docker"  sh -
+```
+### Deploy Workload
+Update `env:SERVER_URL` and apply
+```
+kubectl apply -f ./k8s/deployment.yaml
+```
+Deploys agent pods and two cron-jobs for clean up tasks.
+
+## Drone CI Build
+`./drone/` dir contains a simple TeamCity build project using Drone CI
---
+++ drone/tc-project.kt
@@ -0,0 +1,24 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+
+object Drone_PoC : BuildType({
+    name = "Hello Drone"
+    params {...}
+    vcs {...}
+
+    steps {
+        script {
+            name = "Env"
+            scriptContent = """
+                cat >> .env <<EOL
+                RELEASE_BRANCH=master
+                DRONE_DOCKER_REGISTRY_USERNAME=%Docker.Registry.Username%
+                DRONE_DOCKER_REGISTRY_PASSWORD=%Docker.Registry.Password%
+                EOL
+            """.trimIndent()
+        }
+        script {
+            name = "Drone Exec"
+            scriptContent = "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${'$'}(pwd):${'$'}(pwd) -w ${'$'}(pwd) --env-file .env drone/cli exec --env-file=.env"
+        }
+    }
+})
